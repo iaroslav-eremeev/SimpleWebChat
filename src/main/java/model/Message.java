e71c91b79dd8c4a386a1f81bdd9bdf5095f78956
Iaroslav <eremeev.pt@gmail.com>
2023-04-09 13:21:56 +0300

    Add model package with User and Message classes, add Event class in dto package

---
+++ src/main/java/model/Message.java
@@ -0,0 +1,30 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "messageText", nullable = false)
+    @NonNull
+    private String messageText;
+
+    @ToString.Exclude
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    @Cascade(value = org.hibernate.annotations.CascadeType.DELETE)
+    private User user;
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,42 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"login"})})
+@Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "login", nullable = false, unique = true)
+    @NonNull
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    @NonNull
+    private String password;
+
+    private String hash;
+
+    @ToString.Exclude
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @Cascade(value = org.hibernate.annotations.CascadeType.DELETE)
+    private List<Message> messages = new ArrayList<>();
+
+    public void addMessage(Message message){
+        this.messages.add(message);
+    }
+}
+
