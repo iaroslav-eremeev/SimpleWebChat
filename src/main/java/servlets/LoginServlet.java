e55424cf5fb4a2b60a9905491e298fd5355f6f22
Iaroslav <eremeev.pt@gmail.com>
2023-04-09 13:40:20 +0300

    Add HTML for login and registration

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/login")
-public class UserServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
