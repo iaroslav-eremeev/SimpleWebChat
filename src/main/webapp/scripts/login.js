0ed13d737c35090d5504e7d246e97d37b557dd76
Iaroslav <eremeev.pt@gmail.com>
2023-04-09 13:44:02 +0300

    Add Hibernate config file, login and registration js files and bootstrap

---
+++ src/main/webapp/scripts/login.js
@@ -0,0 +1,19 @@
+$('#btn-go-to-sign-up').click(function () {
+        $(location).attr('href', "http://localhost:8080/SimpleWebChat/registration.html");
+    }
+)
+
+$('#btn-login').click(function () {
+        $.ajax({
+            url: 'login',
+            method: "POST",
+            data: {"login": $('#login').val(), "password": $('#password').val()},
+            success: [function (result) {
+                $(location).attr('href', "http://localhost:8080/SimpleWebChat/index.html");
+            }],
+            error: [function (result) {
+                alert("Wrong login or password!")
+            }]
+        })
+    }
+)
