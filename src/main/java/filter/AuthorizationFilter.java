41ada0dde67edb27091fdb912dcd5d38f0db0526
Iaroslav <eremeev.pt@gmail.com>
2023-04-12 00:01:47 +0300

    Update AuthorizationFilter comments

--- src/main/java/filter/AuthorizationFilter.java
+++ src/main/java/filter/AuthorizationFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebFilter("/*")
+@WebFilter("/*")
 public class AuthorizationFilter implements Filter {
 
     @Override
@@ -19,10 +19,8 @@ public class AuthorizationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-
         Cookie[] cookies = request.getCookies();
         String value = null;
         if (cookies != null) {
@@ -35,14 +33,14 @@ public class AuthorizationFilter implements Filter {
 
         System.out.println(request.getRequestURI());
 
-        //URL Запроса/переадресации на Servlet входа
+        //URL request/redirect to the login Servlet
         String loginURI = request.getContextPath() + "/login";
         String registerURI = request.getContextPath() + "/registration";
-        //Если сессия ранее создана
+        //If a session has been previously created
         boolean loginRequest = request.getRequestURI().contains(loginURI);
         boolean registerRequest = request.getRequestURI().contains(registerURI);
-        //Если запрос пришел со страницы с входом или сессия не пуста даем добро следовать дальше
-        //Если нет ридерект на страницу входа
+        //If the request came from the login page or the session is not empty, allow to proceed
+        //Otherwise, redirect to the login page
         if (request.getRequestURI().endsWith("js") || loginRequest || registerRequest
                 || value != null && DAO.getObjectByParam("hash", value, User.class) != null) {
             filterChain.doFilter(request, response);
@@ -53,6 +51,5 @@ public class AuthorizationFilter implements Filter {
 
     @Override
     public void destroy() {
-
     }
 }
